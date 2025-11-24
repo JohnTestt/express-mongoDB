405199417df0e243ddc2070f8b47bbdb5ef0667c
JohnTestt <jhonathangs25@gmail.com>
2025-11-24 10:12:56 -0300

    correções

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -13,7 +13,7 @@ const setTokenCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // true em prod (https)
-    sameSite: "strict", // protege contra CSRF
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
   });
 };
